59a4ed820c6fec0bb67b842b96e27b3735186812
Dylan Smith <dylan.smith@globaldawn.co>
2011-08-05 17:30:29 +0100

    Added v1.3 (bugfix to support images.google.com domain)

--- contentscript.js
+++ contentscript.js
@@ -1,4 +1,4 @@
-var rgx = new RegExp("^http:\/\/[\w\-]+\.google\.[^\/]+\/imgres\\?imgurl=([^\&]+)", "ig");
+var rgx = new RegExp("^http:\/\/[^\.]+\.google\.[^\/]+\/imgres\\?imgurl=([^\&]+)", "ig");
 var loc = String(window.location);
 var m = rgx.exec(loc);
 if (m != null && m.length == 2) {
