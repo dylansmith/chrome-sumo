25cdfa1843034d566ac65799baf9b8296f74e6a8
Dylan Smith <dylansmith@gmail.com>
2013-10-22 00:44:53 +0100

    Rewrote most of the extension to use a content script and body delegate

--- js/background.js
+++ js/background.js
@@ -1,249 +1,47 @@
-(function() {
+var SumoBackground = {
 
-  var TabLog,
-      TabRegister,
-      register;
+  config: {
+    disabled: false
+  },
 
-  // class definitions
+  init: function() {
+    this.enable();
 
-  TabLog = function(tab) {
-    if (this instanceof TabLog === false) {
-      return new TabLog();
-    }
-    this.tab = tab;
-    this.originalUrl = tab.url;
-    this.override = false;
-  };
-  TabLog.prototype = {
-    // properties
-    originalUrl: null,
-    imageUrl: null,
-    contextPageUrl: null,
-    tab: null,
-    override: null
-  };
-
-  TabRegister = function() {
-    if (this instanceof TabRegister === false) {
-      return new TabRegister();
-    }
-    this.wipe();
-  };
-  TabRegister.prototype = {
-    // properties
-    _store: null,
-    _bypassEnabled: true,
-
-    // methods
-    add: function(tabLog) {
-      var log = this._store[tabLog.tab.id];
-      this._store[tabLog.tab.id] = tabLog;
-    },
-
-    get: function(tabId) {
-      return this._store[tabId] || null;
-    },
-
-    remove: function(tabId) {
-      if (this._store.hasOwnProperty(tabId)) delete this._store[tabId];
-    },
-
-    wipe: function() {
-      this._store = {};
-    },
-
-    bypass: function(tabId) {
-      if (this.bypassEnabled() === false) return;
-      var log = this.get(tabId);
-      if (log && log.imageUrl)
-      {
-        chrome.tabs.get(tabId, function(tab)
-        {
-          // only revert from the orignalUrl
-          if (tab.url !== log.originalUrl) return;
-          chrome.tabs.executeScript(tabId, {code: "window.location='"+log.imageUrl+"';"});
-        });
+    // handle incoming messages from content scripts
+    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+      if (sender.tab && request === 'sumo.get_config') {
+        this.broadcastMessage({'config': this.config});
       }
-    },
+    }.bind(this));
 
-    revert: function(tabId) {
-      var log = this.get(tabId);
-      if (log && log.originalUrl)
-      {
-        chrome.tabs.get(tabId, function(tab)
-        {
-          // only revert from the imageUrl
-          if (tab.url !== log.imageUrl) return;
+    // handle the toolbar toggle button
+    chrome.browserAction.onClicked.addListener(this.toggleDisabledState.bind(this));
+  },
 
-          // set a per-tab override and go back to the Google preview
-          log.override = true;
-          chrome.tabs.executeScript(tabId, {code: "window.location='"+log.originalUrl+"#ignore';"});
-        });
-      }
-    },
+  broadcastMessage: function(message, responseCallback) {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, message, responseCallback);
+    });
+  },
 
-    requestOverride: function(tabId) {
-      var log = this.get(tabId);
-      if (log) {
-        // log.override only lasts for 1 request
-        if (log.override === true) {
-          log.override = false;
-          return true;
-        }
-      }
-      return false;
-    },
+  disable: function() {
+    this.config.disabled = true;
+    chrome.browserAction.setIcon({path: "icons/32off.png"});
+    chrome.browserAction.setTitle({title: 'Disabled: will use normal Google Imagesâ„¢ previews'});
+    this.broadcastMessage({'config': this.config});
+  },
 
-    showContextPage: function(tabId) {
-      var log = this.get(tabId);
-      if (log && log.contextPageUrl) {
-        chrome.tabs.executeScript(tabId, {code: "window.location='"+log.contextPageUrl+"';"});
-      }
-    },
+  enable: function() {
+    this.config.disabled = false;
+    chrome.browserAction.setIcon({path: "icons/32.png"});
+    chrome.browserAction.setTitle({title: 'Enabled: will bypass Google Imagesâ„¢ previews'});
+    this.broadcastMessage({'config': this.config});
+  },
 
-    bypassEnabled: function() {
-      return this._bypassEnabled;
-    },
-
-    enableBypass: function() {
-      if (this.bypassEnabled() === true) return;
-      this._bypassEnabled = true;
-      chrome.browserAction.setIcon({path: "icons/32.png"});
-      chrome.browserAction.setTitle({title: 'Click to disable & use Google Images™ previews'});
-    },
-
-    disableBypass: function() {
-      if (this.bypassEnabled() === false) return;
-      this._bypassEnabled = false;
-      chrome.browserAction.setIcon({path: "icons/32off.png"});
-      chrome.browserAction.setTitle({title: 'Click to enable & bypass Google Images™ previews'});
-    }
-  };
-
-  // implementation
-
-  register = new TabRegister();
-
-  function onTabUpdated(tabId, changeInfo, tab) {
-    if (register.bypassEnabled() === true && tab.status === 'loading') processTab(tab);
+  toggleDisabledState: function() {
+    this[(this.config.disabled) ? 'enable' : 'disable']();
   }
 
-  function processTab(tab)
-  {
-    var log, urls;
+};
 
-    if (!tab.url || tab.url.length < 1) return;
-
-    // respect bypass override on a per-tab basis
-    if (register.requestOverride(tab.id) === true) {
-      return;
-    }
-
-    // check for an existing log
-    log = register.get(tab.id);
-    if (log)
-    {
-      // if we have landed on the google image preview url, and no override
-      // flag existed in the check above, then we need to bypass it either
-      // forwards or backwards, depending on the referrer
-      if (log.originalUrl === tab.url)
-      {
-        chrome.history.search({text:""}, function(results) {
-          var last = results[0];
-          if (last.url === log.imageUrl) {
-            chrome.tabs.executeScript(tabId, {code: "window.history.back()"});
-          }
-          else {
-            chrome.tabs.executeScript(tabId, {code: "window.history.forward()"});
-          }
-        });
-        return;
-      }
-
-      // if the new tab url is the bypassed, pure image url, add the content actions
-      if (log.imageUrl === tab.url) {
-        addContentActions(tab);
-        return;
-      }
-    }
-
-    // test for bypass and create log if found
-    urls = parseUrl(tab.url);
-    if (urls) {
-      log = new TabLog(tab);
-      log.imageUrl = urls.imgurl;
-      log.contextPageUrl = urls.imgrefurl;
-      register.add(log);
-      register.bypass(tab.id);
-    }
-  }
-
-  function addContentActions(tab) {
-    chrome.tabs.insertCSS(tab.id, {file: "css/actions.css"});
-    chrome.tabs.executeScript(tab.id, {file: "js/actions.js"});
-  }
-
-  function parseUrl(url)
-  {
-    var m, rgx, qs, qsh, i, p, imgurl, imgrefurl, output = false;
-
-    if (url.substr(-7, 7) === '#ignore') return;
-    rgx = /^http:\/\/[^\.]+\.google\.[^\/]+\/(imgres|imglanding)\?(.*)$/ig;
-    m = rgx.exec(url);
-    if (m !== null && m.length === 3)
-    {
-      qs = String(m[2]).split('&');
-      qsh = {};
-      for (i in qs) {
-        p = qs[i].split('=');
-        qsh[p[0]] = p[1];
-      }
-      imgurl = qsh[m[1]] || qsh.imgurl || qsh.imglanding || null;
-      imgrefurl = qsh.imgrefurl || null;
-
-      output = {
-        imgurl: imgurl,
-        imgrefurl: imgrefurl
-      };
-    }
-    return output;
-  }
-
-  // handle tab update events
-  chrome.tabs.onUpdated.addListener(onTabUpdated);
-
-  // clear tab logs when the tab is closed
-  chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
-    register.remove(tabId);
-  });
-
-  // handle the toolbar toggle button
-  chrome.browserAction.onClicked.addListener(function(tab) {
-    if (register.bypassEnabled() === true) {
-      register.revert(tab.id);
-      register.disableBypass();
-      register.wipe();
-    } else {
-      register.enableBypass();
-      processTab(tab);
-    }
-  });
-
-  // content script message listener
-  chrome.extension.onMessage.addListener(function(request, sender, sendResponse)
-  {
-    switch (request)
-    {
-      case 'actions.revert':
-        register.revert(sender.tab.id);
-        break;
-
-      case 'actions.context':
-        register.showContextPage(sender.tab.id);
-        break;
-    }
-  });
-
-  register.enableBypass();
-
-}());
+SumoBackground.init();
--- js/content.js
+++ js/content.js
@@ -1,63 +1,74 @@
-var sumo = {
+var SumoContent = {
 
-    isPreviewOpen: null,
+  config: null,
 
-    init: function() {
-        console.log('SUMO content.js loaded', document.body);
-        this.createPreviewObserver();
-    },
-
-    createPreviewObserver: function() {
-        var self = this,
-            previewNode = document.querySelector('#irc_bg'),
-            observer;
-
-        if (previewNode) {
-            observer = new MutationObserver(function(mutations) {
-                var isOpen = (previewNode.offsetHeight > 0);
-                if (isOpen !== self.isPreviewOpen) {
-                    self.isPreviewOpen = isOpen;
-                    if (self.isPreviewOpen === true) {
-                        self.update();
-                    }
-                }
-            });
-
-            observer.observe(previewNode, {
-                attributes: true,
-                subtree: true
-            });
-        }
-    },
-
-    update: function() {
-        var urls = this.parseUrls();
-        if (urls) {
-            document.location = urls[0];
-        }
-    },
-
-    parseUrls: function() {
-        var urls = null,
-            hash = window.location.hash;
-
-        if (/imgrc=/i.test(hash) === true) {
-            hash.split('&').forEach(function(v, i) {
-                var imgUrl, pageUrl, parts = v.split('=');
-                if (parts[0] === 'imgrc') {
-                    // for some reason it's double-encoded
-                    parts[1] = decodeURIComponent(decodeURIComponent(parts[1])).split(';');
-                    urls = [
-                        parts[1][2] || null,
-                        parts[1][3] || null
-                    ];
-                }
-            });
-        }
-
-        return urls;
+  init: function() {
+    // test for a valid url
+    var urlPattern = /^https?:\/\/(.*\.)?google(\.[^\.]+){1,2}\/(search|imgres)\?.*$/ig;
+    if (urlPattern.test(document.location) === false) {
+        return;
     }
+
+    // redirect imgres urls
+    if (document.location.pathname === '/imgres') {
+        // detect url from button
+    }
+
+    // process incoming messages from the extension
+    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+      if ('config' in request) {
+        this.config = request.config;
+        console.log('SUMO config set', this);
+      }
+    }.bind(this));
+
+    // fetch the config initially
+    chrome.runtime.sendMessage('sumo.get_config');
+
+    // create the click observer
+    document.body.addEventListener('click', this.onElementClicked.bind(this), true);
+  },
+
+  /**
+   * Creates a cascading listener on the body to trap relevant click events.
+   */
+  onElementClicked: function(evt) {
+    var el, href;
+    if (this.config.disabled) return;
+
+    el = evt.toElement || evt.target;
+    href = this.parseElement(el);
+    if (href) {
+      evt.stopPropagation();
+      evt.preventDefault();
+      document.location = href;
+    }
+  },
+
+  /**
+   * Parses a given element and returns the target image url if valid.
+   */
+  parseElement: function(el) {
+    var href, matches;
+    if (this.is(el,  'img') && this.is(el.parentNode, 'a[href]')) {
+      href = decodeURIComponent(decodeURIComponent(el.parentNode.getAttribute('href')));
+      matches = /.*\/?imgres\?.*imgurl=([^&;]+).*/gi.exec(href);
+      return (matches && matches[1]) ? matches[1] : false;
+    }
+    return false;
+  },
+
+  /**
+   * Helper method to easily check that a DOM element matches a selector.
+   */
+  is: function(el, selector) {
+    var parent = document.createElement('div'),
+      clone = el.cloneNode(false);
+
+    parent.appendChild(clone);
+    return (parent.querySelector(selector) === clone);
+  }
 
 };
 
-sumo.init();
+SumoContent.init();
